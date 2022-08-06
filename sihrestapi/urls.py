eb197ca8506e93b7b7592b8bffba8ac54df434ed
arhma-rehman <109072922+arhma-rehman@users.noreply.github.com>
2022-08-06 13:51:39 +0530

    first

--- sihrestapi/urls.py
+++ sihrestapi/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path,include
+from django.urls import path, include
 from .router import router
+from django.contrib import admin
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls)
+
 ]
